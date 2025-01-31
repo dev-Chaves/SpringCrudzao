42d0901060a263692a5f5e23f261c8ca93ae1a0c
Joao V Chaves <chavesprogrammer@gmail.com>
2025-01-30 21:03:02 -0300

    comentários no repÃoositório, entendendo melhor

--- src/main/java/academy/chaves/springCurso/repositories/ProductRepository.java
+++ src/main/java/academy/chaves/springCurso/repositories/ProductRepository.java
@@ -8,10 +8,12 @@ import java.util.UUID;
 
 // Alterar para ser uma INTERFACE E NÂO CLASSE
 
-// Extends, errar a classe do JpaRepository, que irá habilitar métodos, como parametro você irá passar seu model(DB) e o ID(UUID) que seja;
+// Extends, a classe do JpaRepository, que irá habilitar métodos, como parametro você irá passar seu model(DB) e o ID(UUID) que seja;
 
 
 //Identificar pro Spring que essa Interface será um bean do tipo repositório, nesse caso o JPA Repository já reconhece, mas deixa essa porra ai, pra não dar erro!
+
+// E Repository é aonde é armazenado os dados, no caso, nosso banco de dados. O Jpa cuida de tudo isso.
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
