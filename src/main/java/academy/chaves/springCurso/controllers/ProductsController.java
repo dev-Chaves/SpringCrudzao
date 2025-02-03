5daa07e5fe35c2d59f538d7270efe39cd856454d
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-03 19:51:19 -0300

    método put adicionado

--- src/main/java/academy/chaves/springCurso/controllers/ProductsController.java
+++ src/main/java/academy/chaves/springCurso/controllers/ProductsController.java
@@ -57,4 +57,21 @@ public class ProductsController {
 
     }
 
+    @PutMapping("products/{id}")
+    public ResponseEntity<Object> updateUpdate(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto){
+
+        Optional<ProductModel> product0 = productRepository.findById(id);
+
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+
+        var productModel = product0.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+
+    }
+
+
 }
