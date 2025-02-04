8f5ceff5ab02a824a0858a8bcbf3348019dd33b2
Joao V Chaves <chavesprogrammer@gmail.com>
2025-02-03 21:51:09 -0300

    método gitId

--- src/main/java/academy/chaves/springCurso/services/ProductsService.java
+++ src/main/java/academy/chaves/springCurso/services/ProductsService.java
@@ -1,13 +1,18 @@
 package academy.chaves.springCurso.services;
 
+import academy.chaves.springCurso.dtos.ProductRecordDto;
 import academy.chaves.springCurso.models.ProductModel;
 import academy.chaves.springCurso.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,5 +28,21 @@ public class ProductsService {
 
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
+
+    public ResponseEntity<Object> listById(@PathVariable(value = "id") UUID id){
+
+        // Optional By id
+
+        Optional<ProductModel> product0 = productRepository.findById(id);
+
+        if (product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("BAD REQUEST");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+
+    }
+
+
 
 }
